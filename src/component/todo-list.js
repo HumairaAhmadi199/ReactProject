354107b4105ff98cdc1ffa43291e665efb736170
HumairaAhmadi199 <humaira.ahmadi199@gmail.com>
2023-10-11 16:19:04 +0430

    first Assignment

---
+++ src/component/todo-list.js
@@ -0,0 +1,11 @@
+import { useState,useEffect } from "react";
+function TodoList(){
+    return(
+         <div className="App">
+            <h1>TodoList</h1>
+            
+
+         </div>
+
+    )
+}
